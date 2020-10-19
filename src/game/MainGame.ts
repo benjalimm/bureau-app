9b2bc4ed92efb6f8984469787d95e083e555d0be
benjalimm <keymote@gmail.com>
2020-10-19 18:28:24 +1030

    Added function to import mesh, figured out animations

--- src/game/MainGame.ts
+++ src/game/MainGame.ts
@@ -1,19 +1,22 @@
-import { FreeCamera, Vector3, HemisphericLight, MeshBuilder, Scene, Engine, Mesh, PickingInfo, AssetsManager, SceneLoader } from '@babylonjs/core'
+import { FreeCamera, Vector3, HemisphericLight, MeshBuilder, Scene, Engine, Mesh, PickingInfo, AssetsManager, SceneLoader, Color3, StandardMaterial, AbstractMesh } from '@babylonjs/core'
 
 export default class MainGame {
-box?: Mesh;
+box?: AbstractMesh;
 camera?: FreeCamera;
 scene?: Scene;
 engine?: Engine;
 assetsManager?: AssetsManager
 
-public initialize (scene: Scene, engine: Engine) {
+groundYLevel: number = 1
+
+
+public async initialize (scene: Scene, engine: Engine) {
   this.scene = scene
   this.engine = engine
   this.camera = new FreeCamera('camera1', new Vector3(0, 5, -10), scene)
   this.assetsManager = new AssetsManager(scene)
   engine.enableOfflineSupport = false;
-  this.prepareScene(scene)
+  await this.prepareScene(scene)
   this.startRenderLoop(engine, scene)
   this.onPointHover(scene, (pickResult) => {
     if (pickResult?.pickedPoint) {
@@ -29,24 +32,24 @@ private startRenderLoop (engine: Engine, scene: Scene) {
   })
 }
 
-private prepareScene (scene: Scene) {
+private async prepareScene (scene: Scene) {
   if (scene.isReady()) {
-    this.onSceneReady(scene)
+    await this.onSceneReady(scene)
   } else {
     scene.onReadyObservable.addOnce(scene => this.onSceneReady(scene))
   }
 }
 
 private onRender (scene: Scene) {
-  if (this.box !== undefined) {
-    var deltaTimeInMillis = scene.getEngine().getDeltaTime()
+  // if (this.box !== undefined) {
+  //   var deltaTimeInMillis = scene.getEngine().getDeltaTime()
 
-    const rpm = 10
-    this.box.rotation.y += ((rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000))
-  }
+  //   const rpm = 10
+  //   this.box.rotation.y += ((rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000))
+  // }
 }
 
-private onSceneReady (scene: Scene) {
+private async onSceneReady (scene: Scene) {
   // This creates and positions a free camera (non-mesh)
 
   // This targets the camera to scene origin
@@ -64,28 +67,30 @@ private onSceneReady (scene: Scene) {
   light.intensity = 0.7
 
   // Our built-in 'box' shape.
-  const box = MeshBuilder.CreateBox('box', { size: 2 }, this.scene) // Move the box upward 1/2 its height
-  box.position.y = 1
+  this.box = MeshBuilder.CreateBox('box', { size: 2 }, this.scene) // Move the box upward 1/2 its height
+
+  const avatar = await this.setObjectOnGround("Suit_Male.babylon", 10, 20, null)
+  avatar.createAnimationRange("Idle", 130, 230)
+  avatar.beginAnimation("Idle", true)
 
   // Our built-in 'ground' shape.
   MeshBuilder.CreateGround('ground', { width: 200, height: 200 }, scene)
 
-  // const coffeeTable = this.assetsManager!.addMeshTask('coffeeTable', 'CoffeeTable.babylon', '../../assets/', 'CoffeeTable.babylon')
-
-  // coffeeTable.onSuccess = (task) => {
-  //   console.log("coffee table has been loaded ")
-  //   task.loadedMeshes[0].position = Vector3.Zero()
-  // }
-
-  // coffeeTable.onError = (task, message, err) => {
-  //   console.log("Failed to load coffee table")
-  //   console.log(err)
-  // }
   SceneLoader.ImportMesh("", "/assets/", "CoffeeTable.babylon", this.scene!, (meshes) => {
     console.log("Loaded meshes")
     meshes[0].position = Vector3.Zero()
     meshes[0].position.y = 1
-  }, (progress) => {
+
+    var myMaterial = new StandardMaterial("myMaterial", scene);
+    
+    myMaterial.diffuseColor = Color3.Blue()
+    myMaterial.specularColor = Color3.Blue()
+    // myMaterial.emissiveColor = Color3.Blue()
+    myMaterial.ambientColor = Color3.Red()
+    
+    
+    
+   }, (progress) => {
 
   }, (scene, message, error) => {
     console.log("failed to load mesh")
@@ -106,9 +111,8 @@ public onClick (scene: Scene) {
   console.log(pickResult)
 
   if (pickResult?.pickedPoint) {
-    const box1 = scene.getMeshByName('box')
-      box1!.position.x = pickResult!.pickedPoint!.x
-      box1!.position.z = pickResult!.pickedPoint!.z
+      this.box!.position.x = pickResult!.pickedPoint!.x
+      this.box!.position.z = pickResult!.pickedPoint!.z
   }
 }
 
@@ -123,20 +127,43 @@ private onPointHover (scene: Scene, onHover: (result: PickingInfo | null) => (vo
   }, 10)
 }
 
-private drawCicleAtPoint (point: Vector3) {
-  const radius = 1
-  var lines = MeshBuilder.CreateDashedLines('lines', {
-    points: [
-      new Vector3(point.x - radius, 0, point.z + radius),
-      new Vector3(point.x + radius, 0, point.z + radius),
-      new Vector3(point.x + radius, 0, point.z - radius),
-      new Vector3(point.x - radius, 0, point.z - radius),
-      new Vector3(point.x - radius, 0, point.z + radius)
-    ]
+  private drawCicleAtPoint (point: Vector3) {
+    const radius = 1
+    var lines = MeshBuilder.CreateDashedLines('lines', {
+      points: [
+        new Vector3(point.x - radius, 0, point.z + radius),
+        new Vector3(point.x + radius, 0, point.z + radius),
+        new Vector3(point.x + radius, 0, point.z - radius),
+        new Vector3(point.x - radius, 0, point.z - radius),
+        new Vector3(point.x - radius, 0, point.z + radius)
+      ]
 
-  })
-  setTimeout(() => {
-    lines.dispose()
-  }, 10)
-}
-}
+    })
+      setTimeout(() => {
+        lines.dispose()
+      }, 10)
+  }
+
+  public setObjectOnGround(name: string, xPosition: number, zPosition: number, onProgress: ((percentage: number) => void) | null): Promise<AbstractMesh> {
+    return new Promise((resolve, reject) => {
+      SceneLoader.ImportMesh("", "/assets/", name, this.scene!, (meshes) => {
+        const mesh = meshes[0]
+        mesh.position = new Vector3(xPosition, this.groundYLevel, zPosition)
+        resolve(mesh)
+      },(progressEvent) => {
+        if (onProgress !== null) {
+          onProgress!(progressEvent.loaded / progressEvent.total)
+        }
+      }, (_, message, error) => {
+        console.log(`Error with setting object on ground: ${message}`)
+        reject(error)
+      })
+    })
+  }
+
+  public foo(name: string, xPosition: number, zPosition: number, onProgress: ((percentage: number) => void) | null) {
+
+
+    
+  }
+} 
